018c3ca67ae803a3a71596b09c4dcc23a29f496a
White Buffalo <apothocare@gmail.com>
2022-12-02 20:03:23 -0600

    Validate function and messages added to Contact Form

--- OneDrive/Desktop/projects/react-portfolio/src/componets/ContactForm/index.js
+++ OneDrive/Desktop/projects/react-portfolio/src/componets/ContactForm/index.js
@@ -1,18 +1,38 @@
 import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function Contact () {
 
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleChange = (e) =>{
-        setFormState({...formState, [e.target.name]: e.target.value})
+        if(e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if(!isValid){
+                setErrorMessage('Your email is invalid');
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                setErrorMessage('');
+            }
+            }
+        }
+        if (!errorMessage){
+        setFormState({...formState, [e.target.name]: e.target.value});
+        }
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formState);
+    }
     console.log(formState);
 
     return(
-        <section>
+        <section className="contentCont">
             <h1>Contact Me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
@@ -27,7 +47,12 @@ function Contact () {
                     <label htmlFor="message">Message:</label>
                     <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5"  />
                 </div>
-                <button type="submit">Submit</button>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <button className="button" type="submit">Submit</button>
             </form>
         </section>
         
