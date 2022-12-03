e36e4bf4b9b2809274ddf34e7b4f622b5f745315
White Buffalo <apothocare@gmail.com>
2022-12-02 22:33:56 -0600

    Setting up projects page with images that render through an Array. Function working correctly, having issues rendering thumbnails

--- OneDrive/Desktop/projects/react-portfolio/src/componets/projects/index.js
+++ OneDrive/Desktop/projects/react-portfolio/src/componets/projects/index.js
@@ -1,25 +1,12 @@
 import React from "react";
-import Project1 from "../../assets/images/Rise-to-the-challenge.png";
-import Project2 from "../../assets/images/Step2Rythymthumbnail.png";
+import ProjectCards from "../ProjectCards";
 
 
 function Projects () {
+
     return (
         <section>
-            <div id="projects" className="mainCont">
-                <div className="contentCont">
-            <h1>Projects</h1>
-                <div className="mainCont">
-                    <div className="projectCard">
-                        <img src={Project1} alt="Portfolio Project Thumbnail"></img>
-                    </div>
-                    <div className="projectCard">
-                        <img src={Project2} alt="Portfolio Project Thumbnail"></img>
-                    </div>
-
-                </div>
-            </div>
-            </div>
+            <ProjectCards/>
         </section>
     );
 }
