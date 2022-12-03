e14f07b8a35e2466a3bd2e88a6bd7ffbf41f85db
White Buffalo <apothocare@gmail.com>
2022-12-02 23:58:26 -0600

    Added github and site links to project page

--- OneDrive/Desktop/projects/react-portfolio/src/componets/ProjectCards/index.js
+++ OneDrive/Desktop/projects/react-portfolio/src/componets/ProjectCards/index.js
@@ -8,53 +8,60 @@ const [projects] = useState([
     {
         name: 'Rise 2 Challenge',
         description: '',
-        link: '',
-        github: ''
+        link: 'https://thawing-atoll-91539.herokuapp.com/',
+        github: 'https://github.com/JayWhiteBuffalo/Rise2Challenge'
     },
     {
         name: 'Step 2 Rhythm',
         description: '',
-        link: '',
-        github: ''
+        link: 'https://jaywhitebuffalo.github.io/Step2Rhythm/',
+        github: 'https://github.com/JayWhiteBuffalo/Step2Rhythm'
     },
     {
         name: 'Egregore',
         description: '',
-        link: '',
-        github: ''
+        link: 'https://github.com/JayWhiteBuffalo/Egregore',
+        github: 'https://github.com/JayWhiteBuffalo/Egregore'
     },
     {
         name: 'J.A.T.E',
         description: '',
-        link: '',
-        github: ''
+        link: 'https://infinite-chamber-71782.herokuapp.com/',
+        github: 'https://github.com/JayWhiteBuffalo/text2Edit'
     },
     {
         name: 'Department Tracker',
         description: '',
-        link: '',
-        github: ''
+        link: 'https://github.com/JayWhiteBuffalo/department-tracker',
+        github: 'https://github.com/JayWhiteBuffalo/department-tracker'
     },
     {
         name: 'E-commerance Back-End',
         description: '',
-        link: '',
-        github: ''
+        link: 'https://github.com/JayWhiteBuffalo/Ecommie',
+        github: 'https://github.com/JayWhiteBuffalo/Ecommie'
     }
 ]);
 
 return (
     <section>
+    <div>
+        <h2 className="center">Here are a few past projects that I've worked on. </h2>
+    </div>
     <div className="mainCont flex-row space-between">
         {projects.map((image, i) => (  
             <div className="projectCard">
-                <h3>{projects[i].name}</h3>
+                <a href = {projects[i].link}>
+                <h3 className="center">{projects[i].name}</h3>
+                </a>
+            <a href = {projects[i].github}>
             <img 
             src={require(`../../assets/images/${i}.png`)}
             alt={projects[i].name}
             className = "img-thumbnail mx-1"
             key={projects[i].name}
             />
+            </a>
     </div>
         ))}
     </div>
