2535b8289cf749834a9dcec183aff27419c364cb
White Buffalo <apothocare@gmail.com>
2022-12-02 23:07:01 -0600

    Thumbnails were not rendering because diffrent file formats (.png, .jpg). Fixed issue and put in place fillers.

--- OneDrive/Desktop/projects/react-portfolio/src/componets/ProjectCards/index.js
+++ OneDrive/Desktop/projects/react-portfolio/src/componets/ProjectCards/index.js
@@ -1,10 +1,4 @@
 import React, { useState } from "react";
-import P1 from "../../assets/images/0.png";
-import P2 from "../../assets/images/1.png";
-import P3 from "../../assets/images/2.jpg";
-import P4 from "../../assets/images/3.png";
-import P5 from "../../assets/images/4.png";
-import P6 from "../../assets/images/5.jpg";
 
 const ProjectCards = () => {
 
@@ -14,55 +8,46 @@ const [projects] = useState([
     {
         name: 'Rise 2 Challenge',
         description: '',
-        image: '../../assets/images/0.png',
         link: '',
         github: ''
     },
     {
         name: 'Step 2 Rhythm',
         description: '',
-        image: P2,
         link: '',
         github: ''
     },
     {
         name: 'Egregore',
         description: '',
-        image: P3,
         link: '',
         github: ''
     },
     {
         name: 'J.A.T.E',
         description: '',
-        image: P4,
         link: '',
         github: ''
     },
     {
         name: 'Department Tracker',
         description: '',
-        image: P5,
         link: '',
         github: ''
     },
     {
         name: 'E-commerance Back-End',
         description: '',
-        image: P6,
         link: '',
         github: ''
     }
 ]);
 
-const [currentCard, setCurrentCard] = useState(projects);
-
-console.log(currentCard[0].name)
 return (
     <section>
-    <div className="">
-        {currentCard.map((i) => (  
-            <div>
+    <div className="mainCont flex-row space-between">
+        {projects.map((image, i) => (  
+            <div className="projectCard">
                 <h3>{projects[i].name}</h3>
             <img 
             src={require(`../../assets/images/${i}.png`)}
@@ -70,7 +55,7 @@ return (
             className = "img-thumbnail mx-1"
             key={projects[i].name}
             />
-            </div>
+    </div>
         ))}
     </div>
     </section>
